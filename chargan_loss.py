fcbd7a23edf65485ccc9daf68cdb850001c39108
Ubuntu <ubuntu@ip-172-31-52-147.us-west-2.compute.internal>
2019-12-07 01:40:32 +0000

    added hparams

--- chargan_loss.py
+++ chargan_loss.py
@@ -11,7 +11,7 @@ import time
 import pathlib
 
 
-BATCH_SIZE = 64
+BATCH_SIZE = 256
 NOISE_DIM = 100
 
 num_to_char = {}
@@ -64,11 +64,13 @@ def make_generator_model(n_classes=10):
     x = layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False)(merge)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
+    # x = tf.keras.layers.Dropout(0.4)(x)
     assert tuple(x.shape) == (None, 7, 7, 128)
 
     x = layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False)(x)
     x = layers.BatchNormalization()(x)
     x = layers.LeakyReLU()(x)
+    # x = tf.keras.layers.Dropout(0.4)(x)
     assert tuple(x.shape) == (None, 14, 14, 64)
 
     # TODO tanh function with output between [-1, 1]
@@ -93,11 +95,11 @@ def make_discriminator_model(n_classes=10):
     # define classification architecture
     x = tf.keras.layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same')(merge)
     x = tf.keras.layers.LeakyReLU()(x)
-    x = tf.keras.layers.Dropout(0.3)(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
 
     x = layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same')(x)
     x = layers.LeakyReLU()(x)
-    x = layers.Dropout(0.3)(x)
+    x = layers.Dropout(0.5)(x)
 
     x = layers.Flatten()(x)
     output = layers.Dense(1)(x)
@@ -121,12 +123,15 @@ def generator_loss(predictions):
 
 
 # Get ground truth data
-# data_dir = tf.keras.utils.get_file('English',
-#                                     origin='http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/EnglishFnt.tgz',
-#                                     untar=True, extract=True)
-# data_dir = pathlib.Path(data_dir)
-# image_path = os.path.join(data_dir, "Img/GoodImg/Bmp")
-image_path = os.path.join("/home/ubuntu/datasets/", "Fnt")
+full_data = True
+if full_data:
+    image_path = os.path.join("/home/ubuntu/datasets/", "Fnt")
+else:
+    data_dir = tf.keras.utils.get_file('English',
+                                        origin='http://www.ee.surrey.ac.uk/CVSSP/demos/chars74k/EnglishFnt.tgz',
+                                        untar=True, extract=True)
+    data_dir = pathlib.Path(data_dir)
+    image_path = os.path.join(data_dir, "Img/GoodImg/Bmp")
 
 image_generator = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=lambda img: (img - 127.5) / 127.5)
 image_ground_truth = image_generator.flow_from_directory(image_path, target_size=(28, 28),
@@ -137,7 +142,7 @@ image_ground_truth = image_generator.flow_from_directory(image_path, target_size
 num_classes = 62
 
 generator = make_generator_model(n_classes=num_classes)
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+generator_optimizer = tf.keras.optimizers.Adam(6e-4)
 discriminator = make_discriminator_model(n_classes=num_classes)
 discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
 
@@ -179,7 +184,7 @@ def train_step(images, ground_truth_labels, update_gen=True):
     return gen_loss, disc_loss_real, disc_loss_fake
 
 
-def train(dataset, epochs, ckpt_prefix):
+def train(dataset, epochs, ckpt_prefix, save_epoch=20, image_epoch=20):
     print("Starting training")
     for epoch in range(epochs):
         start = time.time()
@@ -198,13 +203,10 @@ def train(dataset, epochs, ckpt_prefix):
             if i > len(dataset):
               break
               
-        if (epoch + 1) % 20 == 0:
-            generate_and_save_images(generator, epoch+1, seed, seed_labels)  
+        if (epoch + 1) % image_epoch == 0:
+            generate_and_save_images(generator, epoch+1, seed, seed_labels)
+        if (epoch + 1) % save_epoch == 0:
             checkpoint.save(file_prefix=ckpt_prefix)
-            try:
-                files.download('image_at_epoch_{:04d}.png'.format(epoch+1))
-            except Exception as e:
-                print(e)
                     
         print('Time for epoch {} is {} sec'.format(epoch+1, time.time()-start))
         print('Generator:', average(g_loss), 'Disc Real:', average(d_loss_real), 'Disc Fake:', average(d_loss_fake))
@@ -218,4 +220,4 @@ status = checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 fakes = generator([seed, seed_labels], training=False)
 generate_and_save_images(generator, 0, seed, seed_labels)
 
-train(image_ground_truth, epochs=2000, ckpt_prefix=checkpoint_prefix)
+train(image_ground_truth, epochs=2000, ckpt_prefix=checkpoint_prefix, image_epoch=20)
